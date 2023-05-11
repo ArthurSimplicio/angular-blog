4b149c76c9abd91d93e021f7b5d2c2d54242d937
ArthurSimplicio <arthurpark20@gmail.com>
2023-05-11 14:56:17 -0300

    Atualizado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,50 @@
+export const dataFake = [
+    {
+        "id":1,
+        "title": "James Gunn marcou de movo",
+        "description": "Sem nenhum erro na carreira ainda, James Gunn fez outro ótimo filme tratando de assuntos sérios como: maltratos a animais, colocando como foco principal o Rocket, mostrando suas origens!",
+        "photoCover": "https://cinepop.com.br/wp-content/uploads/2023/04/guardioes-da-galaxia.jpg"
+    },
+    {
+        "id":2,
+        "title": "Filme muito esperado decepcionou a expectativa",
+        "description": "É muito ruim!",
+        "photoCover": "https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/03/14/666375009-0f5b01cf9e4be52649f11c97f270a51d.jpg" 
+    },
+    {
+        "id":3,
+        "title": "'AI MEU MONEY!!!'",
+        "description": "Kevin puxou a carteira e viu que nada entrou!",
+        "photoCover": "https://media.gq-magazine.co.uk/photos/62b2edea2176d0b26a09d519/16:9/w_2560%2Cc_limit/GettyImages-1394210939.jpg" 
+    },
+    {
+        "id":4,
+        "title": "Todos Podemos!",
+        "description": "Brendan Fraser, ápos depressão e façta de esperança, renasce e ganha o seu tão sonhado Oscar!",
+        "photoCover": "https://media-manager.noticiasaominuto.com.br/1920/naom_640eeed01bc95.jpg" 
+    },
+    {
+        "id":5,
+        "title": "Só tenho duas palavras 'para','béns'",
+        "description": "Filme rápido, sem enrolação e diverte do ínicio ao fim",
+        "photoCover": "https://tm.ibxk.com.br/2023/04/13/13132418679158.jpg" 
+    },
+    {
+        "id":6,
+        "title": "Budokai Tekaichi 4 is coming soon",
+        "description": "Após o anúncio Bandai vai ter que caprichar para não decepcionar os fãs do PS2",
+        "photoCover": "https://www.arkade.com.br/wp-content/uploads/2023/03/Dragon-Ball-Z-Budokai-Tenkaichi-4-Goku-Cracking-Knuckles-After-Transformation1-1050x591.jpg" 
+    },
+    {
+        "id":7,
+        "title": "Pode esquecer a física, só lembre do cinto",
+        "description": "Agora com Aquaman vilão terá que ser derrotado com ajuda da Mulher Maravilha em parceria com a Capitã Marvel, queria estar brincando, mas até a Cardi B. e Ludmila confirmaram presença no filme, espero que tenha dinossauros também!",
+        "photoCover": "https://www.rbsdirect.com.br/filestore/3/0/6/3/4/6/4_a022b8755e1633e/4643603_26e412b08111085.jpg?w=700" 
+    },
+    {
+        "id":8,
+        "title": "Agora tá ferrado! kkkkkkk",
+        "description": "Haja café! Arthur terá que cumprir todos os desafios em diferentes linguagens entre elas: Java, JavaScript e Python",
+        "photoCover": "https://www.zdnet.com/a/img/resize/a0dcec40a8cd8d2e1b3a9e12a05c2819622d20be/2021/07/19/8a337c80-5ed6-43a1-98fb-b981d420890f/programming-languages-shutterstock-1680857539.jpg?auto=webp&fit=crop&height=1200&width=1200" 
+    }
+]
